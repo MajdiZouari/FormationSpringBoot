1732cb96a3998ed730511d2dce6df6f5d6bd2d37
Majdi <nassib.haouari@gmail.com>
2019-01-21 12:26:14 +0100

    Initial commit of full repository

---
+++ src/main/java/com/project/projetdatauser/ProjetDataUserApplication.java
@@ -0,0 +1,14 @@
+package com.project.projetdatauser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDataUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDataUserApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/project/projetdatauser/ProjetDataUserApplicationTests.java
@@ -0,0 +1,17 @@
+package com.project.projetdatauser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetDataUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
