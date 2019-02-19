942799e3da615c12cf44876b77e3c6b140c48286
Majdi <nassib.haouari@gmail.com>
2019-02-20 00:31:44 +0100

    Tests Working with RestAssured

--- src/test/java/com/project/projetdatauser/LinkControllerTest.java
+++ src/test/java/com/project/projetdatauser/LinkControllerTest.java
@@ -1,28 +1,22 @@
 package com.project.projetdatauser;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.restassured.RestAssured;
-import com.project.projetdatauser.controller.LinkController;
 import com.project.projetdatauser.model.Link;
-import com.project.projetdatauser.repository.LinkRepository;
 import com.project.projetdatauser.services.LinkService;
-
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
 
 import java.util.Date;
-
+import java.util.HashMap;
+import java.util.Map;
+import static org.assertj.core.api.Assertions.*;
 import static com.jayway.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -30,29 +24,24 @@ import static org.hamcrest.Matchers.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LinkControllerTest {
 
-
     @LocalServerPort
     private int port;
-/*
+
+    @Autowired
+    private LinkService linkService;
+
     private static Link link1;
     private static Link link2;
-
     private static String idLink1, idLink2;
 
-*/
     @BeforeClass
-    public static void setup() {
-        /*linkService.deleteAll();
-        link1 = new Link("link1","shortLink","userId",new Date(2019-01-01));
-        link2 = new Link("link2","shortLink","userId",new Date(2019-02-02));
-        idLink1 = linkService.addLinkToUser(link1);
-        idLink2 = linkService.addLinkToUser(link2);*/
+    public static void setUpBeforeClass() {
         RestAssured.baseURI = "http://localhost/api/v1/links";
         RestAssured.port = 8080;
     }
-/*
+
     @Before
-    public  void  setUp(){
+    public void setUp(){
         linkService.deleteAll();
         link1 = new Link("link1","shortLink","userId",new Date(2019-01-01));
         link2 = new Link("link2","shortLink","userId",new Date(2019-02-02));
@@ -61,17 +50,68 @@ public class LinkControllerTest {
         link1.setLinkId(idLink1);
         link2.setLinkId(idLink2);
     }
-*/
+
+    @Test
+    public void should_success_addLinkToUser(){
+        Map<String,String> link = new HashMap<>();
+        link.put("link", "link");
+        link.put("shortLink", "shortLink");
+        link.put("userId", "userId");
+        link.put("createDate", "1970-01-01T00:00:02.017+0000");
+
+        given()
+                .port(port)
+                .contentType("application/json")
+                .body(link)
+                .when().post(baseURI).then()
+                .statusCode(201);
+    }
+
+    @Test
+    public void should_fail_addLinkToUser(){
+        Map<String,String> link = new HashMap<>();
+        link.put("shortLink", "shortLink");
+        link.put("createDate", "1970-01-01T00:00:02.017+0000");
+
+        given()
+                .port(port)
+                .contentType("application/json")
+                .body(link)
+                .when().post(baseURI).then()
+                .statusCode(400);
+    }
+
     @Test
     public void should_success_getAllLinksByUserId(){
-        given().port(port).when().get("/user/userId")
-                .then().statusCode(200);
+        given()
+                .port(port).when().get("/user/userId")
+                .then().statusCode(200)
+                .assertThat()
+                .body("[0].linkId", equalTo(idLink1))
+                .body("[1].linkId", equalTo(idLink2));
+    }
+
+    @Test
+    public void should_fail_getAllLinksByUserId(){
+        linkService.deleteAll();
+        given()
+                .port(port).when().get("/user/userId")
+                .then().statusCode(404);
     }
 
     @Test
     public void should_success_getLinkById(){
-        given().port(port).when().get("/5c6c5da5acac3758586857b7").then().statusCode(200).assertThat()
-                .body("linkId", equalTo("5c6c5da5acac3758586857b7"));
+        given()
+                .port(port).when().get("/{idLink1}", idLink1)
+                .then().statusCode(200)
+                .assertThat()
+                .body("linkId", equalTo(idLink1));
+    }
 
+    @Test
+    public void should_fail_getLinkById(){
+        given()
+                .port(port).when().get("/wrongLink")
+                .then().statusCode(404);
     }
 }
