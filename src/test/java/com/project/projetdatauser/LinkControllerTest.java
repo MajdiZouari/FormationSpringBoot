3da9f1e1c31b4ab247e0dd7ab837dd643c787e34
Majdi <nassib.haouari@gmail.com>
2019-02-21 02:18:25 +0100

    Integration tests

--- src/test/java/com/project/projetdatauser/LinkControllerTest.java
+++ src/test/java/com/project/projetdatauser/LinkControllerTest.java
@@ -1,5 +1,7 @@
 package com.project.projetdatauser;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.restassured.RestAssured;
 import com.project.projetdatauser.model.Link;
 import com.project.projetdatauser.services.LinkService;
@@ -13,9 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 import static org.assertj.core.api.Assertions.*;
 import static com.jayway.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -30,21 +34,32 @@ public class LinkControllerTest {
     @Autowired
     private LinkService linkService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private static Link link1;
     private static Link link2;
     private static String idLink1, idLink2;
+    private static Date dateLink1, dateLink2;
 
     @BeforeClass
     public static void setUpBeforeClass() {
         RestAssured.baseURI = "http://localhost/api/v1/links";
-        RestAssured.port = 8080;
     }
 
     @Before
     public void setUp(){
         linkService.deleteAll();
-        link1 = new Link("link1","shortLink","userId",new Date(2019-01-01));
-        link2 = new Link("link2","shortLink","userId",new Date(2019-02-02));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            dateLink1 = sdf.parse("2019-01-01");
+            dateLink2 = sdf.parse("2019-02-02");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        link1 = new Link("link1","shortLink","userId",dateLink1);
+        link2 = new Link("link2","shortLink","userId",dateLink2);
         idLink1 = linkService.addLinkToUser(link1);
         idLink2 = linkService.addLinkToUser(link2);
         link1.setLinkId(idLink1);
@@ -53,26 +68,36 @@ public class LinkControllerTest {
 
     @Test
     public void should_success_addLinkToUser(){
-        /**
-         * Je dois pouvoir récupérer dans le header la variable location que j'envoie à la création
-         * Et ensuite examiner le contenu avec les assertThat de AssertJ
-         * Je n'ai pas encore trouvé la bonne syntaxe
-         */
-        Map<String,String> link = new HashMap<>();
-        link.put("link", "link");
-        link.put("shortLink", "shortLink");
-        link.put("userId", "userId");
-        link.put("createDate", "1970-01-01T00:00:02.017+0000");
+        linkService.deleteAll();
+        String locationHeader = "";
+        try {
+            locationHeader =
+                    given()
+                    .port(port)
+                    .contentType("application/json")
+                    .content(objectMapper.writeValueAsString(link1))
+                    .body(link1)
+            .when()
+                    .post(baseURI)
+            .then()
+                    .statusCode(201)
+                            .extract().header("location");
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        Link result =
+                given()
+                        .port(port)
+                        .when()
+                        .get("/{idLink1}", idLink1)
+                        .then()
+                        .statusCode(200)
+                        .assertThat()
+                        .body("linkId", equalTo(idLink1))
+                        .extract().as(Link.class);
 
-        given()
-                .port(port)
-                .contentType("application/json")
-                //.content(objectMapper.writeValueAsString(link)))
-                .body(link)
-        .when()
-                .post(baseURI)
-        .then()
-                .statusCode(201);
+        assertThat(result).isEqualToIgnoringGivenFields(link1, "linkId");
+        assertThat(locationHeader).isEqualTo("http://localhost:"+port+"/api/v1/links/"+idLink1);
     }
 
     @Test
@@ -105,8 +130,18 @@ public class LinkControllerTest {
         .then()
                 .statusCode(200)
                 .assertThat()
-                .body("[0].linkId", equalTo(idLink1))
-                .body("[1].linkId", equalTo(idLink2));
+                    .body("[0].linkId", equalTo(idLink1))
+                    .body("[0].link", equalTo(link1.getLink()))
+                    .body("[0].shortLink", equalTo(link1.getShortLink()))
+                    .body("[0].userId", equalTo(link1.getUserId()))
+                    //.body("[0].createDate", equalTo(dateLink1))
+
+                    .body("[1].linkId", equalTo(idLink2))
+                    .body("[1].link", equalTo(link2.getLink()))
+                    .body("[1].shortLink", equalTo(link2.getShortLink()))
+                    .body("[1].userId", equalTo(link2.getUserId()))
+                    //.body("[1].createDate", equalTo(dateLink2))
+                    ;
     }
 
     @Test
@@ -122,9 +157,6 @@ public class LinkControllerTest {
 
     @Test
     public void should_success_getLinkById(){
-        /**
-         * Ici j'ai réussi à le faire
-         */
         Link result =
                 given()
                     .port(port)
